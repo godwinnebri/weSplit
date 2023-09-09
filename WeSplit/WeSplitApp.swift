13e6a3d2ad42818f29429994b67d0369984b0051
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-09-09 22:46:30 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Godwin IE on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
